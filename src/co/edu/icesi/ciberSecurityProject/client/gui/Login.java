b857e8dae13dfbe981858a9444c04d292d48fc35
unknown <sanpra1208@gmail.com>
2019-06-03 18:10:25 -0500

    Remove executable property from ClientChat.java

--- src/co/edu/icesi/ciberSecurityProject/client/gui/Login.java
+++ src/co/edu/icesi/ciberSecurityProject/client/gui/Login.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package co.edu.icesi.ciberSecurityProject.client.gui;
 
+import co.edu.icesi.ciberSecurityProject.client.gui.ClientChat;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
